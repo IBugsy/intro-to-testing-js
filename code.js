6973cfd383660388d7a16b3c55527f62930a575e
Steven Bugenske <sbugenske@gmail.com>
2023-11-15 16:44:37 -0600

    first testing submission

--- code.js
+++ code.js
@@ -1,4 +1,13 @@
 // helloWorld function 
+// function helloWorld() {
+//     //green: the test identifies as a function
+//     //red: should never return 'undefined' when called
+//     // red:should return a string when called
+//     // red: should return the string "Hello, World!" when executed
+//     console.log("Hello, World!")
+// }
 function helloWorld() {
-    return "Hello, World!";
+    return ("Hello, World!")
 }
+// all green
+
